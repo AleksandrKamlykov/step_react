7e712b36ea60272d75485bce259c6b839823dfcc
Oleksandr Kanlykov <aleksandrkamlykov@gmail.com>
2024-03-08 09:29:37 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,12 @@
-import React, { useState } from 'react'
+import "./App.css";
 
-import './App.css'
-import {TimerInfo} from "./components/TimerInfo.jsx";
+const App = () => {
+  const formatter = new Intl.DateTimeFormat("uk-UA", {
+    minute: "2-digit",
+    second: "2-digit",
+  });
 
-class App extends React.Component{
+  return <div className={"timer"}></div>;
+};
 
-    interval = null;
-    constructor(props) {
-        super(props);
-        this.state ={
-            minutes: 1,
-            seconds: 0,
-            inputValue: 0
-        }
-
-    }
-
-
-    formatter = new Intl.DateTimeFormat("uk-UA",{minute:"2-digit", second:"2-digit"})
-
-    nextStepTomer =()=>{
-        if(this.state.minutes === 0 && this.state.seconds === 0){
-            clearInterval(this.interval)
-        }else if(this.state.seconds === 0){
-            this.setState((prev)=>({...prev, minutes: prev.minutes - 1, seconds: 59}))
-        }else{
-            this.setState((prev)=>({...prev,seconds: prev.seconds - 1}))
-        }
-
-    }
-
-    runTimer=()=>{
-      this.setState((prev)=>({minutes: prev.inputValue, seconds: 0}))
-        if(this.interval){
-            clearInterval(this.interval)
-        }
-        this.interval = setInterval(this.nextStepTomer , 1000);
-    }
-    stopTimer =()=>{
-        clearInterval(this.interval)
-    }
-
-    minutesHandler = (event)=>{
-        const value = +event.target.value > 60 ? 60 : +event.target.value
-        this.setState({inputValue:value})
-    }
-
- render() {
-
-     return (
-         <div className={"timer"}>
-            <TimerInfo time={this.formatter.format(new Date(0,0,0,0,this.state.minutes,this.state.seconds))} />
-             {this.state.minutes === 0 && this.state.seconds === 0 ?<span>Время вышло</span>: null}
-             <input value={this.state.inputValue} onChange={this.minutesHandler} type={"number"} placeholder={"Enter minutes"} />
-             <button onClick={this.runTimer}>run</button>
-             <button onClick={this.stopTimer}>stop</button>
-         </div>
-     )
- }
-}
-
-export default App
+export default App;
